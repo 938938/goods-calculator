abb769d21b99e39f76b6ae55cf0a65ef3aae39f5
938938 <938kjh@gmail.com>
2025-02-27 21:08:17 +0900

    [FIX] 재고 이상 입력되는 오류 수정

--- src/components/home/TableLow.tsx
+++ src/components/home/TableLow.tsx
@@ -9,6 +9,12 @@ const TableLow = ({ item }: { item: ItemType }) => {
   const setGoodsList = useSetRecoilState(listState);
 
   const onCountChangeHandler = (newCount: number) => {
+    if (newCount > item.stock) {
+      newCount = item.stock;
+    } else if (newCount < 0) {
+      newCount = 0;
+    }
+
     setGoodsList((prev) =>
       prev.map((ele) =>
         ele.id === item.id ? { ...ele, count: newCount } : ele
@@ -25,45 +31,45 @@ const TableLow = ({ item }: { item: ItemType }) => {
       </td>
       <td className='p-3 text-right w-1/4'>{item.cost.toLocaleString()}</td>
       <td className='p-3 w-5/12'>
-        <div className='relative'>
-          <div className='absolute left-1 top-1 flex gap-0.5 z-50'>
-            <IconButton
-              size='sm'
-              variant='text'
-              className={`rounded ${item.count === 0 && 'text-gray-200'}`}
-              onClick={() => onCountChangeHandler(Math.max(item.count - 1, 0))}
-              disabled={item.count === 0}
-            >
-              -
-            </IconButton>
+        <div className='flex border rounded-lg gap-2 justify-between'>
+          <IconButton
+            size='lg'
+            variant='text'
+            className={`rounded ${item.count === 0 && 'text-gray-200'}`}
+            onClick={() => onCountChangeHandler(Math.max(item.count - 1, 0))}
+            disabled={item.count === 0}
+          >
+            -
+          </IconButton>
+          <div className='w-14'>
+            <Input
+              type='number'
+              value={item.count}
+              onChange={(e) => onCountChangeHandler(Number(e.target.value))}
+              className={`!border-t-blue-gray-200 placeholder:text-blue-gray-300 placeholder:opacity-100  focus:!border-t-gray-900 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none text-center ${
+                item.count === 0 && 'text-gray-200'
+              } border-0 focus:ring-0 focus:border-0`}
+              labelProps={{
+                className: 'before:content-none after:content-none',
+              }}
+              containerProps={{
+                className: 'h-12 border-0',
+              }}
+            />
           </div>
-          <Input
-            type='number'
-            value={item.count}
-            onChange={(e) => onCountChangeHandler(Number(e.target.value))}
-            className={`!border-t-blue-gray-200 placeholder:text-blue-gray-300 placeholder:opacity-100  focus:!border-t-gray-900 appearance-none [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none text-center ${
-              item.count === 0 && 'text-gray-200'
+          <IconButton
+            size='lg'
+            variant='text'
+            className={`rounded ${
+              item.count === item.stock && 'text-gray-200'
             }`}
-            labelProps={{
-              className: 'before:content-none after:content-none',
-            }}
-            max={item.stock}
-          />
-          <div className='absolute right-1 top-1 flex gap-0.5'>
-            <IconButton
-              size='sm'
-              variant='text'
-              className={`rounded ${
-                item.count === item.stock && 'text-gray-200'
-              }`}
-              onClick={() =>
-                onCountChangeHandler(Math.min(item.count + 1, item.stock))
-              }
-              disabled={item.count === item.stock}
-            >
-              +
-            </IconButton>
-          </div>
+            onClick={() =>
+              onCountChangeHandler(Math.min(item.count + 1, item.stock))
+            }
+            disabled={item.count === item.stock}
+          >
+            +
+          </IconButton>
         </div>
       </td>
     </tr>
