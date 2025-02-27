6403f0c4a995d11735f34183148fec4fa1104d5b
938938 <938kjh@gmail.com>
2025-02-27 14:15:02 +0900

    [FEAT] 판매시 재고 감소 기능 구현

--- src/components/home/SellBtn.tsx
+++ src/components/home/SellBtn.tsx
@@ -1,11 +1,28 @@
-'use client'
+'use client';
 
-import { Button } from "@material-tailwind/react"
+import { editGoods } from '@/actions/item-actions';
+import { listState } from '@/recoil/listState';
+import { Button } from '@material-tailwind/react';
+import { useSetRecoilState } from 'recoil';
 
 const SellBtn = () => {
+  const setGoodsList = useSetRecoilState(listState);
+  const onSellHandler = () => {
+    setGoodsList((prev) => {
+      const update = prev.map((ele) => ({
+        ...ele,
+        stock: ele.stock - ele.count,
+        count: 0,
+      }));
+      editGoods(update);
+      return update;
+    });
+  };
   return (
-    <Button className="bg-orange-900">판매</Button>
-  )
-}
+    <Button onClick={onSellHandler} className='bg-orange-900'>
+      판매
+    </Button>
+  );
+};
 
-export default SellBtn
+export default SellBtn;
