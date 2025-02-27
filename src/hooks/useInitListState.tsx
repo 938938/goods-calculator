b08ab3d6371fd9cbc08009c35984b8ba6577969d
938938 <938kjh@gmail.com>
2025-02-28 00:02:51 +0900

    [FIX] 누락된 데이터 추가

--- src/hooks/useInitListState.tsx
+++ src/hooks/useInitListState.tsx
@@ -10,7 +10,16 @@ const getInitialList = (): ItemType[] => {
     const storedList = localStorage.getItem('list');
     return storedList
       ? JSON.parse(storedList).map((ele: ItemType) => ({ ...ele, count: 0 }))
-      : [{ id: '123', name: '예시 아이템', cost: 1000, count: 0, stock: 10 }];
+      : [
+          {
+            id: '123',
+            name: '예시 아이템',
+            cost: 1000,
+            count: 0,
+            stock: 10,
+            initial: 10,
+          },
+        ];
   } catch (error) {
     console.error('Failed to parse list from localStorage:', error);
     return [
