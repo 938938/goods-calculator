383640ccaaf4650b84da7164bda1981d26f62149
938938 <938kjh@gmail.com>
2025-03-03 19:23:35 +0900

    [FEAT] 이메일 데이터 전송 구현

--- src/api/email.ts
+++ src/api/email.ts
@@ -1,5 +1,6 @@
 'use server';
 
+import { ReceiptType } from '@/model/type';
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
@@ -12,13 +13,60 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-export const sendEmail = async (email: string) => {
+const makeTable = (data: ReceiptType[]) => {
+  return `
+    <p><strong>정산 목록</strong></p>
+    ${data
+      .map(
+        (receipt) => `
+      <div>
+      <p>
+        ${receipt.date}
+      </p>
+      <table  border="1" cellspacing="0" cellpadding="5" style="border-collapse: collapse; width: 100%;">
+        <thead>
+          <tr>
+            <th>상품명</th>
+            <th>갯수</th>
+            <th>단가</th>
+            <th>총합</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${receipt.soldItems
+            .map(
+              (ele) =>
+                `<tr key=${ele.id}>
+              <td>${ele.name}</td>
+              <td>${ele.count}</td>
+              <td>${ele.cost.toLocaleString()}</td>
+              <td>${ele.totalCost.toLocaleString()}</td>
+            </tr>`
+            )
+            .join('')}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>총 갯수</td>
+            <td>${receipt.totalCount}</td>
+            <td>총 합</td>
+            <td>${receipt.result.toLocaleString()}</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+    `
+      )
+      .join('')}`;
+};
+
+export const sendEmail = async (email: string, data: ReceiptType[]) => {
   try {
     await transporter.sendMail({
       from: process.env.NEXT_PUBLIC_GMAIL_USER,
       to: email,
       subject: 'TEST',
-      html: `<p>테스트입니다.</p>`,
+      html: makeTable(data),
     });
 
     return { message: '이메일 전송 성공' };
--- src/components/setting/EmailForm.tsx
+++ src/components/setting/EmailForm.tsx
@@ -1,12 +1,17 @@
 'use client';
 
 import { sendEmail } from '@/api/email';
+import useInitReceiptState from '@/hooks/useInitReceiptState';
+import { receiptState } from '@/recoil/receiptState';
 import { Button, Input } from '@material-tailwind/react';
 import { useState } from 'react';
+import { useRecoilValue } from 'recoil';
 
 const EmailForm = () => {
+  useInitReceiptState();
   const [email, setEmail] = useState<string>('');
-  
+  const receiptList = useRecoilValue(receiptState);
+  console.log(receiptList);
   return (
     <div>
       <Input
@@ -14,7 +19,7 @@ const EmailForm = () => {
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
-      <Button onClick={() => sendEmail(email)}>전송</Button>
+      <Button onClick={() => sendEmail(email, receiptList)}>전송</Button>
     </div>
   );
 };
