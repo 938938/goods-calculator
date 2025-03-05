cdb80d3b9779f4d2f8c26812fde7397139d83cea
938938 <938kjh@gmail.com>
2025-03-05 23:07:07 +0900

    [FEAT] 문의제안 추가

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -1,11 +1,13 @@
+import Ask from '@/components/setting/Ask';
 import DelBtn from '@/components/setting/DelBtn';
 import EmailBtn from '@/components/setting/EmailBtn';
 
 const SettingPage = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-[calc(100svh-70px)] gap-2'>
+    <div className='flex flex-col items-center justify-center h-[calc(100svh-70px)] gap-2 relative'>
       <DelBtn />
       <EmailBtn />
+      <Ask />
     </div>
   );
 };
---
+++ src/components/setting/Ask.tsx
@@ -0,0 +1,5 @@
+const Ask = () => {
+  return <p className="text-gray-700 absolute bottom-2">문의·제안: moss.kim.owo@gmail.com </p>;
+};
+
+export default Ask;
