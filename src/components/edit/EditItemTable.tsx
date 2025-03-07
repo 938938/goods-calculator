9970415b4314a878ba368709f6253da8cb9c0f2f
938938 <938kjh@gmail.com>
2025-03-07 23:08:42 +0900

    [FEAT] 상품이 없는 경우 안내 추가

--- src/components/edit/EditItemTable.tsx
+++ src/components/edit/EditItemTable.tsx
@@ -12,28 +12,35 @@ const EditItemTable = () => {
 
   return (
     <div className='h-[calc(100svh-70px)] flex flex-col pb-16'>
-      <div className='bg-gray-700 text-gray-400 flex'>
-        <div className='w-1/4 p-2 text-left'>상품명</div>
-        <div className='w-1/6 p-2 text-right'>재고</div>
-        <div className='w-1/4 p-2 text-right'>가격</div>
-        <div className='p-2 text-center'></div>
-      </div>
-      <div
-        className='flex-1 overflow-y-auto [&::-webkit-scrollbar]:hidden'
-        style={{
-          scrollbarWidth: 'none',
-          msOverflowStyle: 'none',
-        }}
-      >
-        <table className='table-fixed w-full border-collapse'>
-          <tbody className='bg-white h-20'>
-            {goodsList.map((item) => (
-              <EditTableRow item={item} key={item.id} />
-            ))}
-          </tbody>
-        </table>
-        <ListResetBtn />
-      </div>
+      {goodsList.length ? (
+        <>
+          {' '}
+          <div className='bg-gray-700 text-gray-400 flex'>
+            <div className='w-1/4 p-2 text-left'>상품명</div>
+            <div className='w-1/6 p-2 text-right'>재고</div>
+            <div className='w-1/4 p-2 text-right'>가격</div>
+            <div className='p-2 text-center'></div>
+          </div>
+          <div
+            className='flex-1 overflow-y-auto [&::-webkit-scrollbar]:hidden'
+            style={{
+              scrollbarWidth: 'none',
+              msOverflowStyle: 'none',
+            }}
+          >
+            <table className='table-fixed w-full border-collapse'>
+              <tbody className='bg-white h-20'>
+                {goodsList.map((item) => (
+                  <EditTableRow item={item} key={item.id} />
+                ))}
+              </tbody>
+            </table>
+            <ListResetBtn />
+          </div>
+        </>
+      ) : (
+        <p className='text-center w-full my-4'>굿즈를 추가해주세요!</p>
+      )}
     </div>
   );
 };
