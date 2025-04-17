abfffcf38e016ace05c7b7f888d47dcc690f6480
938938 <938kjh@gmail.com>
2025-04-17 22:32:01 +0900

    [FEAT] 기본 데이터 수정

--- src/hooks/useInitListState.tsx
+++ src/hooks/useInitListState.tsx
@@ -5,33 +5,74 @@ import { listState } from '@/recoil/listState';
 import { useEffect } from 'react';
 import { useSetRecoilState } from 'recoil';
 
+const initialData = [
+  {
+    id: '1',
+    name: '굿즈 계산 및 재고 관리!',
+    cost: 0,
+    count: 0,
+    stock: 10,
+    initial: 10,
+  },
+  {
+    id: '2',
+    name: '체크된 총 금액은 아래에!',
+    cost: 1000,
+    count: 0,
+    stock: 10,
+    initial: 10,
+  },
+  {
+    id: '3',
+    name: '판매 버튼 : 정산에 기록!',
+    cost: 2000,
+    count: 0,
+    stock: 10,
+    initial: 10,
+  },
+  {
+    id: '4',
+    name: '초기화 : 기록 없이 초기화!',
+    cost: 3000,
+    count: 0,
+    stock: 10,
+    initial: 10,
+  },
+  {
+    id: '5',
+    name: '수정 : 굿즈 입력 및 수정!',
+    cost: 4000,
+    count: 0,
+    stock: 10,
+    initial: 10,
+  },
+  {
+    id: '6',
+    name: '정산 : 판매내역 확인 가능!',
+    cost: 5000,
+    count: 0,
+    stock: 10,
+    initial: 10,
+  },
+  {
+    id: '7',
+    name: '설정 : 이메일 전송 가능!',
+    cost: 10000,
+    count: 0,
+    stock: 10,
+    initial: 10,
+  },
+];
+
 const getInitialList = (): ItemType[] => {
   try {
     const storedList = localStorage.getItem('list');
     return storedList
       ? JSON.parse(storedList).map((ele: ItemType) => ({ ...ele, count: 0 }))
-      : [
-          {
-            id: '123',
-            name: '예시 아이템',
-            cost: 1000,
-            count: 0,
-            stock: 10,
-            initial: 10,
-          },
-        ];
+      : initialData;
   } catch (error) {
     console.error('Failed to parse list from localStorage:', error);
-    return [
-      {
-        id: '123',
-        name: '예시 아이템',
-        cost: 1000,
-        count: 0,
-        stock: 10,
-        initial: 10,
-      },
-    ];
+    return initialData;
   }
 };
 
