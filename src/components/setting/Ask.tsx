23e8b0cbc8376b7375dbb13a11a4f723e592f94b
938938 <938kjh@gmail.com>
2025-08-05 00:25:51 +0900

    [STYLE] 안내문구 추가

--- src/components/setting/Ask.tsx
+++ src/components/setting/Ask.tsx
@@ -1,5 +1,12 @@
 const Ask = () => {
-  return <p className="text-gray-700 absolute bottom-2">문의·제안: moss.kim.owo@gmail.com </p>;
+  return (
+    <div className='absolute bottom-2 text-center'>
+      <p className='text-orange-800 text-sm'>
+        안정적인 사용을 위해 해당 사이트는 탭 하나로만 이용해주세요.
+      </p>
+      <p className='text-gray-700'>문의·제안: moss.kim.owo@gmail.com </p>
+    </div>
+  );
 };
 
 export default Ask;
